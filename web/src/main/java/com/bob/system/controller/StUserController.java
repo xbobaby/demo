32054ff511635e847c978f8744aabb7997bc0393
xbobaby <2290200557@qq.com>
2018-10-24 22:34:50 +0800

    添加redis缓存

--- web/src/main/java/com/bob/system/controller/StUserController.java
+++ web/src/main/java/com/bob/system/controller/StUserController.java
@@ -3,11 +3,16 @@ package com.bob.system.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.bob.common.service.RedissionService;
+import com.bob.common.util.RedissonUtils;
 import com.bob.common.vo.RO;
 import com.bob.system.entity.StUser;
 import com.bob.system.service.IStUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,6 +36,10 @@ public class StUserController {
     @Reference(version = "${bob.service.version}")
     IStUserService stUserService;
 
+    @Autowired
+    RedissionService redissionService;
+
+
     @PostMapping(value = "/allusers", produces = {"application/json;charset=UTF-8"})
     public RO getAllUser(){
         List list = stUserService.listMaps(null);
@@ -42,5 +51,12 @@ public class StUserController {
     public RO<StUser> getUserByDeptId(StUser user){
         Page<StUser> page = stUserService.selectUserByDeptId(new Page<>(1, 1),user);
         return new RO(page);
+    }
+
+    @PostMapping(value = "/redisTest", produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "redis缓存测试" , notes = "查询redis缓存" )
+    public RO<StUser> redisTest(String cacheKey){
+//        RBucket<Object> keyObject = RedissonUtils.getRBucket(redissonClient, cacheKey);
+        return new RO().put("object",redissionService.get(cacheKey));
     }
 }
