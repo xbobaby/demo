3042bed19fe1c33fa74e4289e1ea4c12e1c881db
xbobaby <2290200557@qq.com>
2018-11-01 19:55:48 +0800

    添加删除缓存的测试方法

--- web/src/main/java/com/bob/system/controller/StUserController.java
+++ web/src/main/java/com/bob/system/controller/StUserController.java
@@ -3,19 +3,18 @@ package com.bob.system.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.bob.common.constant.ERRORCODE;
 import com.bob.common.service.RedissionService;
-import com.bob.common.util.RedissonUtils;
 import com.bob.common.vo.RO;
 import com.bob.system.entity.StUser;
 import com.bob.system.service.IStUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.redisson.api.RBucket;
-import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,6 +31,7 @@ import java.util.List;
 @RequestMapping("/system/st-user")
 @Api(value = "StUserController|用户数据操作的控制器")
 public class StUserController {
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Reference(version = "${bob.service.version}")
     IStUserService stUserService;
@@ -54,9 +54,21 @@ public class StUserController {
     }
 
     @PostMapping(value = "/redisTest", produces = {"application/json;charset=UTF-8"})
-    @ApiOperation(value = "redis缓存测试" , notes = "查询redis缓存" )
+    @ApiOperation(value = "查询redis缓存测试" , notes = "查询redis缓存" )
     public RO<StUser> redisTest(String cacheKey){
 //        RBucket<Object> keyObject = RedissonUtils.getRBucket(redissonClient, cacheKey);
         return new RO().put("object",redissionService.get(cacheKey));
+    }
+
+    @PostMapping(value = "/redisDeleteTest", produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "删除redis缓存测试" , notes = "删除redis缓存" )
+    public RO<StUser> redisDeleteTest(String cacheKey){
+        logger.info("删除redis缓存,key:{}",cacheKey);
+        if (redissionService.delete(cacheKey)){
+            return new RO();
+        }else{
+            return new RO().setError(ERRORCODE.FAILED);
+        }
+
     }
 }
